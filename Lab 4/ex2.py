095d2ef122f872c3acfa99d0866d8ce7ccdbfd06
mmswflow-upb <152873071+mmswflow-upb@users.noreply.github.com>
2025-03-28 03:10:15 +0200

    Finished explaining lab 4

--- Lab 4/ex2.py
+++ Lab 4/ex2.py
@@ -36,11 +36,25 @@ x = |0 1|
 x = np.array([ [0,0], [0,1], [1,0], [1,1] ])
 T = np.array([0,1,1,0])
 
-#
-# other_T is just T with extra steps
 
-#MLP (Multiple Layer Perceptron) Classifier
-model = nn.MLPClassifier(hidden_layer_sizes=(), random_state= 1, max_iter= 500)
+"""
+MLP (Multiple Layer Perceptron) Classifier
+
+it creates a neural network of 2 layers with one hidden layer of 4 neurons in this case, and it makes
+it run for 4000 times at most in order to find the optimal solution. Like mentioned before, for the XOR function
+we need a hidden layer, not just one layer with 1 or 2 perceptrons..
+
+the hidden_layer_sizes=() parameter takes a tuple of numbers, ith number 
+in the tuple represents number of neurons or perceptrons in ith layer
+
+Make sure that when you add a hidden layer its size is between: (no_features + no_classes)/2  and  2*(no_features) 
+
+then we fit the data (train the model) and we predict
+
+model.score gives how accurate are the results in percentage, we're obviously aiming for 100% accuracy,
+i.e. all elements of the y vector match the true label vector values
+"""
+model = nn.MLPClassifier(hidden_layer_sizes=(4,), random_state= 1, max_iter= 4000)
 model.fit(x, T)
 Y = model.predict(x)
 
